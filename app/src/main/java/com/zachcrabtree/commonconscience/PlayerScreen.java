0f1b52f3974dabb7108c11ca9cdd54c84768b0ab
zakattak <zachary.jay.crabtree@gmail.com>
2015-12-18 23:18:00 -0800

    tries to construct a custom listener class to allow buttons on screen to perform different actions

--- app/src/main/java/com/zachcrabtree/commonconscience/PlayerScreen.java
+++ app/src/main/java/com/zachcrabtree/commonconscience/PlayerScreen.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
--- app/src/main/java/com/zachcrabtree/commonconscience/charAction.java
+++ app/src/main/java/com/zachcrabtree/commonconscience/charAction.java
@@ -1,16 +1,12 @@
 package com.zachcrabtree.commonconscience;
 
-/**
- * Created by zakcrabtree on 12/18/15.
- */
 public class charAction {
 
-    private int energyMod, happyMod, stressMod;
+    private int energyMod, happyMod, stressMod, timeIncrement;
 
     public int getEnergyMod() { return energyMod; }
     public int getHappyMod() { return happyMod; }
     public int getStressMod() { return stressMod; }
-
-    
+    public int getTimeIncrement() { return timeIncrement;}
 
 }
