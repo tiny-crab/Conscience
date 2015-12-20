9569ebde409524883b0a871f0a591fa0bb5da9d9
zakattak <zachary.jay.crabtree@gmail.com>
2015-12-19 18:35:25 -0800

    deletes actionlistener class, start of a custom button class

---
+++ app/src/main/java/com/zachcrabtree/commonconscience/actionButton.java
@@ -0,0 +1,11 @@
+package com.zachcrabtree.commonconscience;
+
+import android.widget.Button;
+
+/**
+ * Created by zakcrabtree on 12/19/15.
+ */
+public class actionButton extends Button{
+
+    private charAction action;
+}
