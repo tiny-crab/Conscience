ec43cd277b2886546997e786feb785cf9450277f
zakattak <zachary.jay.crabtree@gmail.com>
2015-12-21 13:38:35 -0800

    adds new code to fragment and new referencing in the array resource

--- app/src/main/java/com/zachcrabtree/commonconscience/actionButtonFragment.java
+++ app/src/main/java/com/zachcrabtree/commonconscience/actionButtonFragment.java
@@ -6,27 +6,52 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by zakcrabtree on 12/21/15.
- */
 public class actionButtonFragment extends Fragment {
 
     private String[] actionArray = new String[]{};
+    private int[] modArray = new int[]{};
 
-    public void onCreate()
-    {
-        //get all of the strings necessary to fill 6 buttons
-        //eventually, this will be randomly generated, and finally, habitually generated
-        actionArray = getResources().getStringArray(R.array.actionList);
-        //populate buttons with resources
-
-    }
-
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        getStringArray();
         return inflater.inflate(R.layout.fragment_actionbuttons, container, false);
     }
 
     public void modifyVals(View view) {
-        
+        //switch case for each of six buttons
+        //if button[x] is pressed, get the string out of actionArray[x]
+        //then push all items of the string in actionArray[x] into modArray
+
+        String actionToTake;
+        switch (view.getId()) {
+            case R.id.action0:
+                actionToTake = actionArray[0];
+                break;
+            case R.id.action1:
+                actionToTake = actionArray[1];
+                break;
+            case R.id.action2:
+                actionToTake = actionArray[2];
+                break;
+            case R.id.action3:
+                actionToTake = actionArray[3];
+                break;
+            case R.id.action4:
+                actionToTake = actionArray[4];
+                break;
+            case R.id.action5:
+                actionToTake = actionArray[5];
+                break;
+            default:
+                //throw an error?
+        }
+
+
+    }
+
+    private void getStringArray(){
+        //get all of the strings necessary to fill 6 buttons
+        //eventually, this will be randomly generated, and finally, habitually generated
+        actionArray = getResources().getStringArray(R.array.actionList);
     }
 }
