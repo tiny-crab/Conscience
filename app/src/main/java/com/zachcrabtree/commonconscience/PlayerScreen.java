fa9440a9c2911ff1b93d6ad714008c7b6eb59e68
zakattak <zachary.jay.crabtree@gmail.com>
2015-12-30 10:42:25 -0800

    adds color changes

--- app/src/main/java/com/zachcrabtree/commonconscience/PlayerScreen.java
+++ app/src/main/java/com/zachcrabtree/commonconscience/PlayerScreen.java
@@ -1,13 +1,9 @@
 package com.zachcrabtree.commonconscience;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class PlayerScreen extends ActionBarActivity {
 
@@ -16,6 +12,36 @@ public class PlayerScreen extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_screen);
         getSupportActionBar().hide();
+
+        //storing references to the values to manipulate later
+        TextView energyValue = (TextView) findViewById(R.id.energyVal);
+        TextView stressValue = (TextView) findViewById(R.id.stressVal);
+        TextView happyValue = (TextView) findViewById(R.id.happyVal);
+
+        energyValue.setText("100");
+        stressValue.setText("100");
+        happyValue.setText("100");
+        colorText(energyValue);
+        colorText(stressValue);
+        colorText(happyValue);
     }
+
+    private void colorText(TextView targetView) {
+        //gets the energy, stress, or happiness value as a number
+        int value = Integer.parseInt(targetView.getText().toString());
+        if(value == 100) {
+            targetView.setTextColor(Color.GREEN);
+        }
+        if(value < 100 && value >= 50)
+        {
+            targetView.setTextColor(Color.YELLOW);
+        }
+        if(value < 50 && value >= 0)
+        {
+            targetView.setTextColor(Color.RED);
+        }
+    }
+
+
 
 }
